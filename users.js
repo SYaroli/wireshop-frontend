43eec8466512b370abba46c5499c6308508aa9b4
SYaroli <shaneyaroli@gmail.com>
2025-07-14 11:20:54 -0400

    Add files via upload

---
+++ users.js
@@ -0,0 +1,8 @@
+// Basic user database
+const users = [
+  { username: "shane", pin: "1234", role: "admin" },
+  { username: "rafaela", pin: "1234", role: "assembler" },
+  { username: "michael", pin: "1234", role: "tech" },
+  { username: "larissa", pin: "1234", role: "assembler" },
+  { username: "tyler", pin: "1234", role: "assembler" }
+];
