699d91191df6fead8da14421d26727eced6a6d09
SYaroli <shaneyaroli@gmail.com>
2025-07-15 06:27:54 -0400

    users.js

--- users.js
+++ users.js
@@ -2,7 +2,8 @@
 const users = [
   { username: "shane", pin: "1234", role: "admin" },
   { username: "rafaela", pin: "1234", role: "assembler" },
-  { username: "michael", pin: "1234", role: "tech" },
+  { username: "michael", pin: "1234", role: "assembler" },
   { username: "larissa", pin: "1234", role: "assembler" },
   { username: "tyler", pin: "1234", role: "assembler" }
+  { username: "carter", pin: "1234", role: "assembler" }
 ];
